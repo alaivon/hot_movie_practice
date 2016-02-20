2445ca832cb0b37d05c6474dc7dbee755e1bb9c4
= <kingliin814@gmail.com>
2016-02-20 22:25:30 +0800

    migrate rating to comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220132616) do
+ActiveRecord::Schema.define(version: 20160220142505) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "type"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160220132616) do
     t.integer  "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "rating"
   end
 
   create_table "movies", force: :cascade do |t|
