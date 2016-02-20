08d2c4c7dafe2db0161a26bcf07b13009ddc9b73
= <kingliin814@gmail.com>
2016-02-20 21:31:28 +0800

    generate comment issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220091400) do
+ActiveRecord::Schema.define(version: 20160220132616) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "user_id"
+    t.integer  "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
